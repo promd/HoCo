bc907b78223fd50426d226249ca89f9599d146bd
ToSa27 <tobias@die-hoffs.net>
2017-01-02 10:41:00 +0100

    phase 1 script update

--- setup_root.sh
+++ setup_root.sh
@@ -1,13 +1,21 @@
 #! /bin/bash
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y dist-upgrade
-sudo apt-get clean
-sudo useradd -m hoco
-sudo su -c "echo 'hoco ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/hoco"
-sudo adduser hoco dialout
-sudo adduser hoco gpio
-sudo passwd hoco
-sudo su -c "echo 'HOCO_HOME=/opt/hoco' >> /etc/environment"
-sudo reboot
 
+export HOCO_USER=hoco
+export HOCO_HOME=/opt/hoco
+
+apt-get -y update
+apt-get -y upgrade
+apt-get -y dist-upgrade
+apt-get clean
+useradd -m $HOCO_USER
+echo $HOCO_USER' ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/hoco
+adduser $HOCO_USER dialout
+adduser $HOCO_USER gpio
+passwd $HOCO_USER
+echo 'HOCO_USER='$HOCO_USER >> /etc/environment
+echo 'HOCO_HOME='$HOCO_HOME >> /etc/environment
+mkdir -p $HOCO_HOME
+cd $HOCO_HOME
+mv "${0%/*}" $HOCO_HOME/setup
+chown -R $HOCO_USER:$HOCO_USER $HOCO_HOME
+reboot
