cbfac7133c3d540fba6badc0b63c313c5e3ca45b
Tobias Hoff <ToSa27@users.noreply.github.com>
2017-01-05 22:19:09 +0100

    Update setup_adapter.sh

--- setup_adapter.sh
+++ setup_adapter.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
-
+. /etc/environment
 whiptail --yesno "Install HoCo ZWave Adapter?" --defaultyes 20 60 2
 if [ $? -eq 0 ]; then
-    /opt/hoco/setup/setup_zwave.sh
+    $HOCO_HOME/setup/setup_zwave.sh
 fi
 
 whiptail --yesno "Install HoCo Homematic Adapter?" --defaultyes 20 60 2
 if [ $? -eq 0 ]; then
-    /opt/hoco/setup/setup_homematic.sh
+    $HOCO_HOME/setup/setup_homematic.sh
 fi
 
 echo 'HOCO_NEXT_SCRIPT=setup_done.sh' > $HOCO_HOME/setup/status
