6b81a794d402f142dc86f6f4c5520fac4b5ff3a0
Tobias Hoff <ToSa27@users.noreply.github.com>
2017-01-05 22:14:55 +0100

    Update setup_update.sh

--- setup_update.sh
+++ setup_update.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-dpkg -l > packages.old
+dpkg -l > /opt/hoco/setup/packages.old
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
-dpkg -l > packages.new
-cmp --silent packages.old packages.new && echo 'HOCO_NEXT_SCRIPT=setup_hoco.sh' > $HOCO_HOME/setup/status.sh
-rm packages.old
-rm packages.new
+dpkg -l > /opt/hoco/setup/packages.new
+cmp --silent packages.old packages.new && echo 'HOCO_NEXT_SCRIPT=setup_hoco.sh' > /opt/hoco/setup/status.sh
+rm /opt/hoco/setup/packages.old
+rm /opt/hoco/setup/packages.new
 sudo reboot
