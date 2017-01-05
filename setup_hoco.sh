35c22d733f2ec95f17dd5f5ee876cb117d4413b0
ToSa27 <tobias@die-hoffs.net>
2017-01-05 11:46:27 +0100

    cleanup

--- setup_hoco.sh
+++ setup_hoco.sh
@@ -5,33 +5,33 @@ mkdir $HOCO_HOME/adapter
 mkdir $HOCO_HOME/controller
 mkdir $HOCO_HOME/logic
 mkdir $HOCO_HOME/historian
-echo 'export HOCO_MQTT_URL=' > $HOCO_HOME/data/config.sh
-echo 'export HOCO_MQTT_USER=' >> $HOCO_HOME/data/config.sh
-echo 'export HOCO_MQTT_PASS=' >> $HOCO_HOME/data/config.sh
-echo 'export HOCO_MQTT_PREFIX=hoco' >> $HOCO_HOME/data/config.sh
-nano $HOCO_HOME/data/config.sh
+HOCO_MQTT_URL=$(whiptail --inputbox "Please enter MQTT URL" 20 60 3>&1 1>&2 2>&3)
+HOCO_MQTT_USER=$(whiptail --inputbox "Please enter MQTT User" 20 60 "hoco" 3>&1 1>&2 2>&3)
+HOCO_MQTT_PASS=$(whiptail --inputbox "Please enter MQTT Password" 20 60 3>&1 1>&2 2>&3)
+HOCO_MQTT_PREFIX=$(whiptail --inputbox "Please enter MQTT Prefix" 20 60 "hoco" 3>&1 1>&2 2>&3)
+echo 'export HOCO_MQTT_URL='${HOCO_MQTT_URL}'' > $HOCO_HOME/data/config.sh
+echo 'export HOCO_MQTT_USER='${HOCO_MQTT_USER}'' >> $HOCO_HOME/data/config.sh
+echo 'export HOCO_MQTT_PASS='${HOCO_MQTT_PASS}'' >> $HOCO_HOME/data/config.sh
+echo 'export HOCO_MQTT_PREFIX='${HOCO_MQTT_PREFIX}'' >> $HOCO_HOME/data/config.sh
+chmod 755 $HOCO_HOME/data/config.sh
 curl -sL https://deb.nodesource.com/setup_6.x | sudo bash -
 sudo apt-get -y install nodejs
-sudo npm install -g pm2
-pm2 dump
-sudo pm2 startup systemd -u $HOCO_USER
-pm2 dump
-pm2 save
-read -p "Install ZWave Adapter?" yn
+read -p "Install ZWave Adapter? " yn
 case $yn in
   [Yy]* )
     $HOCO_HOME/setup/setup_zwave.sh
-    break;;
+    ;;
   * )
     exit
     ;;
 esac
-read -p "Install Homematic Adapter?" yn
+read -p "Install Homematic Adapter? " yn
 case $yn in
   [Yy]* )
     $HOCO_HOME/setup/setup_homematic.sh
-    break;;
+    ;;
   * )
     exit
     ;;
 esac
+sudo reboot
--- setup_root.sh
+++ setup_root.sh
@@ -1,5 +1,27 @@
 #! /bin/bash
 
+if [ $(id -u) -ne 0 ]; then
+  printf "Script must be run as root.\n"
+  exit 1
+fi
+
+CURRENT_HOSTNAME=`cat /etc/hostname | tr -d " \t\n\r"`
+NEW_HOSTNAME=$(whiptail --inputbox "Please enter a hostname" 20 60 "$CURRENT_HOSTNAME" 3>&1 1>&2 2>&3)
+if [ $? -eq 0 ]; then
+  echo $NEW_HOSTNAME > /etc/hostname
+  sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$NEW_HOSTNAME/g" /etc/hosts
+fi
+whiptail --msgbox "You will now be asked to enter a new password for the pi user" 20 60 1
+passwd pi &&
+whiptail --msgbox "Password changed successfully" 20 60 1
+dpkg-reconfigure tzdata
+update-rc.d ssh enable &&
+invoke-rc.d ssh start &&
+sed -i /boot/cmdline.txt -e "s/console=ttyAMA0,[0-9]\+ //"
+sed -i /boot/cmdline.txt -e "s/console=serial0,[0-9]\+ //"
+echo "enable_uart=0" >> /boot/config.txt
+raspi-config --expand-rootfs
+
 export HOCO_USER=hoco
 export HOCO_HOME=/opt/hoco
 
@@ -9,16 +31,20 @@ apt-get -y dist-upgrade
 apt-get -y update
 apt-get -y upgrade
 apt-get -y dist-upgrade
+apt-get -y install git
 apt-get clean
 useradd -m $HOCO_USER
 echo $HOCO_USER' ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/hoco
 adduser $HOCO_USER dialout
 adduser $HOCO_USER gpio
-passwd $HOCO_USER
+whiptail --msgbox "You will now be asked to enter a new password for the $HOCO_USER user" 20 60 1
+passwd $HOCO_USER &&
+whiptail --msgbox "Password changed successfully" 20 60 1
 echo 'HOCO_USER='$HOCO_USER >> /etc/environment
 echo 'HOCO_HOME='$HOCO_HOME >> /etc/environment
 mkdir -p $HOCO_HOME
 cd $HOCO_HOME
-mv "${0%/*}" $HOCO_HOME/setup
+git clone https://github.com/ToSa27/HoCo.git $HOCO_HOME/setup
+echo "1" > $HOCO_HOME/setup/status
 chown -R $HOCO_USER:$HOCO_USER $HOCO_HOME
 reboot
