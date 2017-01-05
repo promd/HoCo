1ece8e09c22d9d360c0452928e9607470be1ffaf
Tobias Hoff <ToSa27@users.noreply.github.com>
2017-01-05 13:03:37 +0100

    Update setup_zwave.sh

--- setup_zwave.sh
+++ setup_zwave.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 
 git clone https://github.com/ToSa27/HoCo-Adapter-ZWave.git $HOCO_HOME/adapter/zwave
-cp $HOCO_HOME/adapter/zwave/config-template.sh $HOCO_HOME/adapter/zwave/config.sh
-nano $HOCO_HOME/adapter/zwave/config.sh
 $HOCO_HOME/adapter/zwave/setup.sh
