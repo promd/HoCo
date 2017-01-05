e8ffed9a077590e209020859a7423282cd7900b6
Tobias Hoff <ToSa27@users.noreply.github.com>
2017-01-05 13:03:56 +0100

    Update setup_homematic.sh

--- setup_homematic.sh
+++ setup_homematic.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 
 git clone https://github.com/ToSa27/HoCo-Adapter-Homematic.git $HOCO_HOME/adapter/homematic
-cp $HOCO_HOME/adapter/homematic/config-template.sh $HOCO_HOME/adapter/homematic/config.sh
-nano $HOCO_HOME/adapter/homematic/config.sh
 $HOCO_HOME/adapter/homematic/setup.sh
